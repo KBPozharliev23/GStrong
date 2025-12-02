d21e93f76a477e369d36c61f168616f4722c34c5
GZPavlov <GZPavlov23@codingburgas.bg>
2025-12-02 20:55:25 +0200

    Update navigation bar

--- GStrong/app/(tabs)/_layout.tsx
+++ GStrong/app/(tabs)/_layout.tsx
@@ -4,14 +4,29 @@ import { Ionicons } from "@expo/vector-icons"
 
 const TabsLayout = () => {
     return (
-      <Tabs screenOptions={{}}>
+      <Tabs screenOptions={{
+        tabBarActiveTintColor: '#5caed3',
+        tabBarStyle:{
+            backgroundColor: "#252525",
+            borderTopWidth: 2,
+            borderTopColor: "#5caed3",
+            height: 90,
+            paddingBottom: 30,
+            paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "600",
+        },
+        headerShown: false,
+      }}>
 
         <Tabs.Screen 
         name="index"
         options={{
             title:"Home",
             tabBarIcon: ({color,size}) => (
-                <Ionicons name='home-outline'/>
+                <Ionicons name='home-outline' size={size} color={color}/>
             )
         }}
         />
@@ -20,7 +35,7 @@ const TabsLayout = () => {
         options={{
             title:"Bingo",
             tabBarIcon: ({color,size}) => (
-                <Ionicons name='home-outline'/>
+                <Ionicons name='grid-outline' size={size} color={color}/>
             )
         }}
         />
